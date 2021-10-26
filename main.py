37b74f36bc1fd8da1172adba4cb0239a4dc86af9
elliottkobelansky <elliottkobelansky@gmail.com>
2021-10-25 22:49:49 -0400

    Initial Files

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have loggen in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    msg = message.content.split(" ")
+
+    if message.content.startswith('!wca'):
+        if msg[1] == "wr":
+            if msg[2] == "single":
+                await message.channel.send("15.27")
+            if msg[2] == "mean":
+                await message.channel.send("18.18")
+
+client.run('OTAyMzYzNjUwNTU0MjIwNTU2.YXdVow.xdV0N5EkoFhQLvb6rZ_ReMbHo5g')
+
